423247a86819305fabe423278e0d8e9dda988d8f
38096 <05lozaverlan@gmail.com>
2024-01-21 13:24:53 +0100

    version 1.3

--- main.py
+++
@@ -1,8 +0,0 @@
-nachalo = int(input("начало- "))
-konec = int(input("конец- "))
-for num in range(max(2, nachalo), konec + 1):
-    for i in range(max(2, num):
-        if num % i == 0:
-            break
-    else:
-        print(num, end=" ")
