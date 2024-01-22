729f2aeb9cdcda77f9a64c73ef30bd1b15535ef8
38096 <05lozaverlan@gmail.com>
2024-01-22 16:36:29 +0100

    figura_6

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 l = 6
-for i in range(l, 0, -1):
-    print(" " * (l - 1 - i) + "*" * (1 + i * 2))
-else:
-    l = 6
-    for i in range(l):
-        print(" " * (l - 1 - i) + "*" * (1 + i * 2))
+for i in range(l):
+       print(" " * i + "*" * (l - i))
