c5e5534e6d2eb922f013bffc4eb6908ad72c07c6
38096 <05lozaverlan@gmail.com>
2024-01-22 17:37:52 +0100

    figura_1

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 l = 8
 for i in range(1, l + 1):
-    print(("" * (l - i) + "*" * (i)) + ("  " * (l - i) + "*" * (i)))
-else:
-   l = 8
-for i in range(l):
-    print(("" * i + "*" * (l - i)) + ("  " * i + "*" * (l - i)))
+    print("" * (l - i) + "*" * (i))
